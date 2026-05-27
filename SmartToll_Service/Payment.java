2a8856aead2c5a626ec66851b6bd1112c97a287a
iankitdubey12 <ankit0dubey123@gmail.com>
2026-05-27 12:19:49 +0530

    Delete SmartToll_Service directory

--- SmartToll_Service/Payment.java
+++
@@ -1,6 +0,0 @@
-package SmartToll_Service;
-public class Payment{
-    public void process(String VehicleNo,double amount){
-        System.out.println("Payment Successful for "+VehicleNo+" Amount: "+amount);
-    }
-}
